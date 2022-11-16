a3d7e20042b782312bb13213b6afcb6ba1d6c2e9
phan huu cuong <phanhuucuong05012001@gmail.com>
2022-11-16 15:56:40 +0700

    v1,v2 : config mysql

---
+++ v2/config/configMysql.js
@@ -0,0 +1,11 @@
+require('dotenv').config();
+module.exports={
+    json :function(){
+        return {
+            host        : process.env.MYSQL_HOST,
+            user        : process.env.MYSQL_USER,
+            password    : process.env.MYSQL_PASSWORD,
+            database    : process.env.MYSQL_DB
+        };
+    }
+}
