29f1553ced4dd39b2232c1c6d018f346e98ed402
phan huu cuong <phanhuucuong05012001@gmail.com>
2022-11-18 01:31:49 +0700

    v2: edit config redis

--- v2/app.js
+++ v2/app.js
@@ -40,8 +40,8 @@ app.use(checkSession);
 rejson(redis);
 
 const redisClient = redis.createClient({
-  host: '127.0.0.1',
-  port: 6379
+  host: process.env.HOST_REDIS,
+  port: process.env.PORT_REDIS
 });
 
 
