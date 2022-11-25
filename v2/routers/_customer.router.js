66d633721642319fd70be70013c7c8d7ea5289c9
phan huu cuong <phanhuucuong05012001@gmail.com>
2022-11-26 06:08:59 +0700

    fix

--- v2/routers/_customer.router.js
+++ v2/routers/_customer.router.js
@@ -75,7 +75,7 @@ module.exports = {
             phone   :req.body.phone
         };
 
-        var customerInfoByID = await customerModel.getOne({condition});
+        var customerInfoByID = await customerModel.getOne(condition);
 
         if(customerInfoByID.length==0){
             return res.json({status:201,message:"update thong tin khong thanh cong"});
